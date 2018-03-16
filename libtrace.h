b97b4b04d5cd8e7ddbbd1e7a70f76282ecd35d06
Seimizu Joukan <saimizi@gmail.com>
2018-03-17 00:08:30 +0900

    Fix coding style.

--- libtrace.h
+++ libtrace.h
@@ -3,9 +3,9 @@
 
 #define MAX_TRACE_MSG_SIZE	1024
 
-void trace_msg(const char *fmt,...);
+void trace_msg(const char *fmt, ...);
 void trace_backtrace(void);
-void trace_backtrace_sym(const char * fn);
+void trace_backtrace_sym(const char *fn);
 #define trace_backtrace()	trace_backtrace_sym(NULL)
 
 #endif /*__LIBTRACE_H__*/
