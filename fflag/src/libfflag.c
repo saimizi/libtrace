bced5f98562341ab88134b3550a7a9bc1487342d
Seimizu Joukan <saimizi@gmail.com>
2019-01-26 15:49:13 +0900

    Add error log for libfflag.

--- fflag/src/libfflag.c
+++ fflag/src/libfflag.c
@@ -7,8 +7,19 @@
 #include <fcntl.h>
 #include <string.h>
 #include <limits.h>
+#include <errno.h>
 
 #include <sys/inotify.h>
+
+#define libfflag_err(fmt, ...) \
+	fprintf(stderr, "[libfflag] %s() Error: "fmt, __func__, ## __VA_ARGS__)
+
+#ifdef DEBUG
+#define libfflag_dbg(fmt, ...) \
+	fprintf(stderr, "[libfflag] %s() Debug: "fmt, __func__, ## __VA_ARGS__)
+#else
+#define libfflag_dbg(fmt, ...)
+#endif
 
 
 #define FFLAG_DIR		"/tmp/fflag"
@@ -36,11 +47,38 @@ static int is_fflag_dir_ok(const char *dir)
 			break;
 		}
 
-		if (!S_ISDIR(st.st_mode))
+		if (!S_ISDIR(st.st_mode)) {
+			libfflag_err("FFlag directory %s is invalid.\n", dir);
 			break;
+		}
 
+#if 0
 		if (!is_fflag_dir_perm_ok(st.st_mode))
 			break;
+#endif
+
+		ret = 1;
+
+	} while (0);
+
+	return ret;
+}
+
+int is_flag_ok(const char *flag)
+{
+	int ret = 0;
+
+	do {
+		if (!flag) {
+			libfflag_err("Flag is NULL.\n");
+			break;
+		}
+
+		if (strlen(flag) > MAX_FLAG_SIZE) {
+			libfflag_err("Flag %s is too long (%d > MAX=%d).\n",
+				flag, strlen(flag), MAX_FLAG_SIZE);
+			break;
+		}
 
 		ret = 1;
 
@@ -78,10 +116,7 @@ int wait_flag_on(const char *flag)
 		int inotifyFd;
 		struct stat	st;
 
-		if (!flag)
-			break;
-
-		if (strlen(flag) > MAX_FLAG_SIZE)
+		if (!is_flag_ok(flag))
 			break;
 
 		if (initcheck() < 0)
@@ -89,15 +124,23 @@ int wait_flag_on(const char *flag)
 
 
 		event_buf = malloc(EVENT_BUF_LEN);
-		if (!event_buf)
+		if (!event_buf) {
+			libfflag_err("No memeory.\n");
 			break;
+		}
 
 		inotifyFd = inotify_init();
-		if (inotifyFd < 0)
+		if (inotifyFd < 0) {
+			libfflag_err("Failed to init inotify(%d):%s.\n",
+					errno, strerror(errno));
 			break;
+		}
 
-		if (inotify_add_watch(inotifyFd, FFLAG_DIR, IN_CREATE) < 0)
+		if (inotify_add_watch(inotifyFd, FFLAG_DIR, IN_CREATE) < 0) {
+			libfflag_err("Failed to add inotify watch (%d):%s.\n",
+					errno, strerror(errno));
 			break;
+		}
 
 		/* Flag has already been set */
 		sprintf(buf, "%s/%s", FFLAG_DIR, flag);
@@ -111,8 +154,15 @@ int wait_flag_on(const char *flag)
 			char *p;
 
 			numRead = read(inotifyFd, event_buf, EVENT_BUF_LEN);
-			if (numRead < 0)
+			if (numRead < 0) {
+				if (errno == EAGAIN || errno == EINTR)
+					continue;
+
+				libfflag_err("Failed to read inotify events (%d): %s.\n",
+					errno, strerror(errno));
+				ret = -2;
 				break;
+			}
 
 			if (numRead == 0)
 				continue;
@@ -148,10 +198,7 @@ int wait_flag_off(const char *flag)
 	do {
 		struct stat	st;
 
-		if (!flag)
-			break;
-
-		if (strlen(flag) > MAX_FLAG_SIZE)
+		if (!is_flag_ok(flag))
 			break;
 
 		if (initcheck() < 0)
@@ -159,18 +206,26 @@ int wait_flag_off(const char *flag)
 
 
 		event_buf = malloc(EVENT_BUF_LEN);
-		if (!event_buf)
+		if (!event_buf) {
+			libfflag_err("No memeory.\n");
 			break;
+		}
 
 		int inotifyFd;
 
 		inotifyFd = inotify_init();
-		if (inotifyFd < 0)
+		if (inotifyFd < 0) {
+			libfflag_err("Failed to init inotify(%d):%s.\n",
+					errno, strerror(errno));
 			break;
+		}
 
 		if (inotify_add_watch(inotifyFd,
-			FFLAG_DIR, IN_DELETE | IN_DELETE_SELF) < 0)
+			FFLAG_DIR, IN_DELETE | IN_DELETE_SELF) < 0) {
+			libfflag_err("Failed to add inotify watch (%d):%s.\n",
+					errno, strerror(errno));
 			break;
+		}
 
 		/* Flag is not set yet */
 		sprintf(buf, "%s/%s", FFLAG_DIR, flag);
@@ -184,8 +239,15 @@ int wait_flag_off(const char *flag)
 			char *p;
 
 			numRead = read(inotifyFd, event_buf, EVENT_BUF_LEN);
-			if (numRead < 0)
+			if (numRead < 0) {
+				if (errno == EAGAIN || errno == EINTR)
+					continue;
+
+				libfflag_err("Failed to read inotify events(%d): %s.\n",
+					errno, strerror(errno));
+				ret = -2;
 				break;
+			}
 
 			if (numRead == 0)
 				continue;
@@ -202,6 +264,8 @@ int wait_flag_off(const char *flag)
 						break;
 					}
 				} else {
+					libfflag_err("FFLAG_DIR %s dispeared.\n",
+							FFLAG_DIR);
 					ret = -2;
 					break;
 				}
@@ -226,10 +290,7 @@ int set_flag(const char *flag)
 	do {
 		struct stat	st;
 
-		if (!flag)
-			break;
-
-		if (strlen(flag) > MAX_FLAG_SIZE)
+		if (!is_flag_ok(flag))
 			break;
 
 		if (initcheck() < 0)
@@ -242,8 +303,16 @@ int set_flag(const char *flag)
 			break;
 		}
 
-		if (open(buf, O_CREAT, 0744) == -1)
+		if (open(buf, O_CREAT, 0744) == -1) {
+			if (errno == EEXIST) {
+				ret = 0;
+				break;
+			}
+
+			libfflag_err("Failed to create FLAG %s (%d): %s.\n",
+					flag, errno, strerror(errno));
 			break;
+		}
 
 		ret = 0;
 
@@ -261,10 +330,7 @@ int clear_flag(const char *flag)
 	do {
 		struct stat	st;
 
-		if (!flag)
-			break;
-
-		if (strlen(flag) > MAX_FLAG_SIZE)
+		if (!is_flag_ok(flag))
 			break;
 
 		if (initcheck() < 0)
@@ -277,13 +343,21 @@ int clear_flag(const char *flag)
 			break;
 		}
 
-		if (unlink(buf) == -1)
-		break;
+		if (unlink(buf) == -1) {
+			if (errno == ENOENT) {
+				ret = 0;
+				break;
+			}
 
-	ret = 0;
+			libfflag_err("Failed to clear flag (%d): %s.\n",
+				errno, strerror(errno));
+			break;
+		}
 
-} while (0);
+		ret = 0;
 
-return ret;
+	} while (0);
+
+	return ret;
 }
 
--- fflag/src/libfflag.h
+++ fflag/src/libfflag.h
@@ -5,5 +5,6 @@ int wait_flag_on(const char *flag);
 int wait_flag_off(const char *flag);
 int set_flag(const char *flag);
 int clear_flag(const char *flag);
+int is_flag_ok(const char *flag);
 
 #endif
